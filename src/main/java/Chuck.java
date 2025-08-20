86b8df20e8b8b912c82682352b6e509bcd14c6ca
Venu <thevorld864@gmail.com>
2025-08-21 00:08:07 +0800

    Level-0

---
+++ src/main/java/Chuck.java
@@ -0,0 +1,9 @@
+public class Chuck {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________\n");
+        System.out.println("Hey! I'm Chuck, short for Charlie.\n");
+        System.out.println("How can I help you?\n");
+        System.out.println("____________________________________________________________\n");
+        System.out.println("See you around!");
+    }
+}
