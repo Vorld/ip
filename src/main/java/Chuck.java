af87f1fab7db075325348153b853b7edc8ed9e16
Venu <thevorld864@gmail.com>
2025-08-21 00:46:24 +0800

    Level-1

--- src/main/java/Chuck.java
+++ src/main/java/Chuck.java
@@ -4,6 +4,15 @@ public class Chuck {
         System.out.println("Hey! I'm Chuck, short for Charlie.\n");
         System.out.println("How can I help you?\n");
         System.out.println("____________________________________________________________\n");
+
+        String input = "";
+        while (!input.equals("bye")) {
+            Scanner io = new Scanner(System.in);
+            input = io.nextLine();
+            System.out.println(input);
+            System.out.println("____________________________________________________________\n");
+        }
+
         System.out.println("See you around!");
     }
 }
